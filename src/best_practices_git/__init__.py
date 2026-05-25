43de671602fb6bcf16bf36958b4c1cb785bb03f7
Paul Skeffington <36854171+pskeffington@users.noreply.github.com>
2026-05-25 19:26:55 -0400

    Add Python package skeleton

---
+++ src/best_practices_git/__init__.py
@@ -0,0 +1,5 @@
+"""Workflow helpers for publishable machine-learning manuscripts."""
+
+from .workflow import ManuscriptStage, ManuscriptWorkflow
+
+__all__ = ["ManuscriptStage", "ManuscriptWorkflow"]
